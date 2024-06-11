ebbf8139e390b1655cf8446c692cc162b6ab528b
HimanshuM10 <97388658+HimanshuM10@users.noreply.github.com>
2024-06-11 23:35:16 +0530

    Add files via upload

---
+++ components/providers/RootProviders.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { ThemeProvider } from 'next-themes'
+import React, { ReactNode } from 'react'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
+
+function RootProviders({children}:{children:ReactNode}) {
+  const queryClient = new QueryClient();
+  return (
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>{children}</ThemeProvider>
+    <ReactQueryDevtools initialIsOpen={false}/>
+  </QueryClientProvider>
+  )
+}
+
+export default RootProviders
